012e3ebafce28d8e0ec5ab91b40f21847912b967
Varun Sharma <varunsharmalive@gmail.com>
2019-08-15 21:01:08 +0530

    Bump version.

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 
 setup(
     name='auditlog',
-    version='0.0.1',
+    version='0.0.2',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
     url='https://github.com/gmware/auditlog',
     license='MIT',
     author='Gmware',
     description='Based on django-auditlog with additional features for auditing logs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'django-jsonfield>=1.0.0',
         'python-dateutil==2.6.0'
